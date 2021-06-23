203f4bd5961fec4ec7311dc54c0790e7956933b2
Eric Schmidt <eric.schmidt1350@gmail.com>
2021-06-23 07:30:32 -0400

    Load user object into homepage/Redirect

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import baseUrl from '../utils/baseUrl';
 
-function Index({ posts }) {
-  return (
-    <div>
-      {posts &&
-        posts.length > 0 &&
-        posts.map((post) => <h1 key={post._id}>{post.title}</h1>)}
-    </div>
-  );
+function Index({ user, userFollowStats }) {
+  console.log({ user, userFollowStats });
+  return <div>Homepage</div>;
 }
 
-Index.getInitialProps = async (ctx) => {
-  try {
-    const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-
-    const { name } = ctx.query;
-
-    return { posts: res.data };
-  } catch (error) {
-    return { errorLoading: true };
-  }
-};
 export default Index;
--- utils/authUser.js
+++ utils/authUser.js
@@ -36,6 +36,15 @@ export const loginUser = async (user, setError, setLoading) => {
   setLoading(false);
 };
 
+export const redirectUser = (ctx, location) => {
+  if (ctx.req) {
+    ctx.res.writeHead(302, { Location: location });
+    ctx.res.end();
+  } else {
+    Router.push(location);
+  }
+};
+
 const setToken = (token) => {
   cookie.set('token', token);
   Router.push('/');

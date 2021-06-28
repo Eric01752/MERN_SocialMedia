c63c8556c41455112d0f9c7c5193ddf58fcfaf8c
Eric Schmidt <eric.schmidt1350@gmail.com>
2021-06-28 17:49:56 -0400

    Profile routes followers/following

--- api/profile.js
+++ api/profile.js
@@ -63,4 +63,122 @@ router.get('/posts/:username', authMiddleware, async (req, res) => {
   }
 });
 
+//Get followers
+router.get('/followers/:userId', authMiddleware, async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await FollowerModel.findOne({ user: userId }).populate(
+      'followers.user'
+    );
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    return res.json(user.followers);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+});
+
+//Get following
+router.get('/following/:userId', authMiddleware, async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await FollowerModel.findOne({ user: userId }).populate(
+      'following.user'
+    );
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    return res.json(user.following);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+});
+
+//Follow a user
+router.post('/follow/:userToFollowId', authMiddleware, async (req, res) => {
+  const { userId } = req;
+  const { userToFollowId } = req.params;
+
+  try {
+    const user = await FollowerModel.findOne({ user: userId });
+    const userToFollow = await FollowerModel.findOne({ user: userToFollowId });
+    if (!user || !userToFollow) {
+      return res.status(404).send('User not found');
+    }
+
+    const isFollowing =
+      user.following.length > 0 &&
+      user.following.filter(
+        (following) => following.user.toString() === userToFollowId
+      ).length > 0;
+
+    if (isFollowing) {
+      return res.status(401).send('User already followed');
+    }
+
+    await user.following.unshift({ user: userToFollowId });
+    await user.save();
+
+    await userToFollow.followers.unshift({ user: userId });
+    await userToFollow.save();
+
+    return res.status(200).send('Success');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+});
+
+//Unfollow a user
+router.put('/unfollow/:userToUnfollowId', authMiddleware, async (req, res) => {
+  const { userId } = req;
+  const { userToUnfollowId } = req.params;
+
+  try {
+    const user = await FollowerModel.findOne({ user: userId });
+    const userToUnfollow = await FollowerModel.findOne({
+      user: userToUnfollowId,
+    });
+    if (!user || !userToUnfollow) {
+      return res.status(404).send('User not found');
+    }
+
+    const isFollowing =
+      user.following.length > 0 &&
+      user.following.filter(
+        (following) => following.user.toString() === userToUnfollowId
+      ).length === 0;
+
+    if (isFollowing) {
+      return res.status(401).send('User not followed previously');
+    }
+
+    const removeFollowing = user.following
+      .map((following) => following.user.toString())
+      .indexOf(userToUnfollowId);
+
+    await user.following.splice(removeFollowing, 1);
+    await user.save();
+
+    const removeFollower = userToUnfollow.followers
+      .map((follower) => follower.user.toString())
+      .indexOf(userId);
+
+    await userToUnfollow.followers.splice(removeFollower, 1);
+    await userToUnfollow.save();
+
+    return res.status(200).send('Success');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;

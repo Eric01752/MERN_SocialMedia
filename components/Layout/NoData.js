64de3b772f25df259fc4546e6110e90da28c2f75
Eric Schmidt <eric.schmidt1350@gmail.com>
2021-07-12 16:19:30 -0400

    Follower Notification component

--- components/Layout/NoData.js
+++ components/Layout/NoData.js
@@ -1,20 +1,38 @@
-import { Message, Button } from "semantic-ui-react";
+import { Message, Button } from 'semantic-ui-react';
 
 export const NoProfilePosts = () => (
   <>
-    <Message info icon="meh" header="Sorry" content="User has not posted anything yet!" />
-    <Button icon="long arrow alternate left" content="Go Back" as="a" href="/" />
+    <Message
+      info
+      icon='meh'
+      header='Sorry'
+      content='User has not posted anything yet!'
+    />
+    <Button
+      icon='long arrow alternate left'
+      content='Go Back'
+      as='a'
+      href='/'
+    />
   </>
 );
 
 export const NoFollowData = ({ followersComponent, followingComponent }) => (
   <>
     {followersComponent && (
-      <Message icon="user outline" info content="User does not have followers" />
+      <Message
+        icon='user outline'
+        info
+        content='User does not have followers'
+      />
     )}
 
     {followingComponent && (
-      <Message icon="user outline" info content="User does not follow any users" />
+      <Message
+        icon='user outline'
+        info
+        content='User does not follow any users'
+      />
     )}
   </>
 );
@@ -22,21 +40,29 @@ export const NoFollowData = ({ followersComponent, followingComponent }) => (
 export const NoMessages = () => (
   <Message
     info
-    icon="telegram plane"
-    header="Sorry"
-    content="You have not messaged anyone yet.Search above to message someone!"
+    icon='telegram plane'
+    header='Sorry'
+    content='You have not messaged anyone yet.Search above to message someone!'
   />
 );
 
 export const NoPosts = () => (
   <Message
     info
-    icon="meh"
-    header="Hey!"
-    content="No Posts. Make sure you have followed someone."
+    icon='meh'
+    header='Hey!'
+    content='No Posts. Make sure you have followed someone.'
   />
 );
 
 export const NoProfile = () => (
-  <Message info icon="meh" header="Hey!" content="No Profile Found." />
+  <Message info icon='meh' header='Hey!' content='No Profile Found.' />
+);
+
+export const NoNotifications = () => (
+  <Message content='No Notifications' icon='smile' info />
+);
+
+export const NoPostFound = () => (
+  <Message info icon='meh' header='Hey!' content='No Post Found.' />
 );

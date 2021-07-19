3acd02fa28a08387cc3027ac5b8074b55e20e05b
Eric Schmidt <eric.schmidt1350@gmail.com>
2021-07-19 16:17:07 -0400

    MessageInputField component

--- components/Messages/MessageInputField.js
+++ components/Messages/MessageInputField.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Segment } from 'semantic-ui-react';
 
-function MessageInputField() {
-  return <div></div>;
+function MessageInputField({ socket, user, messagesWith }) {
+  const [text, setText] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div style={{ position: 'sticky', bottom: '0' }}>
+      <Segment secondary color='teal' attached='bottom'>
+        <Form reply onSubmit={(e) => e.preventDefault()}>
+          <Form.Input
+            size='large'
+            placeholder='Send New Message'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            action={{
+              color: 'blue',
+              icon: 'telegram place',
+              disabled: text === '',
+              loading: loading,
+            }}
+          />
+        </Form>
+      </Segment>
+    </div>
+  );
 }
 
 export default MessageInputField;
